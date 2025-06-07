b315204c779de2fa555ca31f05c0e5016f4a0307
mambo <mambobryan@gmail.com>
2025-06-07 16:09:38 +0300

    adds publish work

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -1,11 +1,19 @@
 import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import java.util.Properties
 
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
     alias(libs.plugins.android.library)
     alias(libs.plugins.compose.multiplatform)
     alias(libs.plugins.compose.compiler)
+    alias(libs.plugins.maven.publish)
+}
+
+fun Project.findProperties(file: String): Properties {
+    val properties = Properties()
+    properties.load(project.rootProject.file(file).reader())
+    return properties
 }
 
 kotlin {
@@ -57,5 +65,18 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+publishing {
+    val properties = findProperties("local.properties")
+    repositories {
+        maven {
+            setUrl("https://maven.pkg.github.com/bizilabs/shady")
+            credentials {
+                username = properties["github.username"].toString()
+                password = properties["github.token"].toString()
+            }
+        }
     }
 }
