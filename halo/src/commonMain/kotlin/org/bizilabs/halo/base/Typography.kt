fce1de09954e6fb59d5e31b6b6666225ea9a9e3d
Waitara <janewaitara99@gmail.com>
2025-07-09 17:57:06 +0300

    Theme:Typography - Updates the tokens to have labels

--- halo/src/commonMain/kotlin/org/bizilabs/halo/base/Typography.kt
+++ halo/src/commonMain/kotlin/org/bizilabs/halo/base/Typography.kt
@@ -13,38 +13,43 @@ import androidx.compose.ui.unit.em
 import androidx.compose.ui.unit.sp
 
 @Immutable
-data class HaloTypography internal constructor (
+data class HaloTypography internal constructor(
     private val privateFontFamily: FontFamily,
 
     val titleLarge: TextStyle = TextStyle(
         fontFamily = privateFontFamily,
-        fontWeight = FontWeight.Medium,
+        fontWeight = FontWeight.SemiBold,
         fontSize = 36.sp,
         lineHeight = 52.sp,
         letterSpacing = (-0.5).em
     ),
     val titleMedium: TextStyle = TextStyle(
         fontFamily = privateFontFamily,
-        fontWeight = FontWeight.Medium,
+        fontWeight = FontWeight.SemiBold,
         fontSize = 32.sp,
         lineHeight = 44.sp
     ),
     val titleSmall: TextStyle = TextStyle(
         fontFamily = privateFontFamily,
-        fontWeight = FontWeight.Medium,
+        fontWeight = FontWeight.SemiBold,
         fontSize = 24.sp,
         lineHeight = 32.sp
     ),
     val subTitle: TextStyle = TextStyle(
         fontFamily = privateFontFamily,
-        fontWeight = FontWeight.Medium,
+        fontWeight = FontWeight.SemiBold,
         fontSize = 20.sp,
         lineHeight = 28.sp,
-        letterSpacing = (-0.01).em
     ),
     val bodyLargeBold: TextStyle = TextStyle(
         fontFamily = privateFontFamily,
         fontWeight = FontWeight.Medium,
+        fontSize = 18.sp,
+        lineHeight = 24.sp
+    ),
+    val bodyLarge: TextStyle = TextStyle(
+        fontFamily = privateFontFamily,
+        fontWeight = FontWeight.Normal,
         fontSize = 18.sp,
         lineHeight = 24.sp
     ),
@@ -56,28 +61,40 @@ data class HaloTypography internal constructor (
     ),
     val bodyMedium: TextStyle = TextStyle(
         fontFamily = privateFontFamily,
-        fontWeight = FontWeight.Light,
+        fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp
     ),
-    val bodySmall: TextStyle = TextStyle(
+    val bodySmallBold: TextStyle = TextStyle(
         fontFamily = privateFontFamily,
-        fontWeight = FontWeight.Light,
+        fontWeight = FontWeight.Medium,
         fontSize = 14.sp,
         lineHeight = 20.sp
     ),
-    val bodyExtraSmall: TextStyle = TextStyle(
+    val bodySmall: TextStyle = TextStyle(
         fontFamily = privateFontFamily,
-        fontWeight = FontWeight.Light,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp,
+        lineHeight = 20.sp
+    ),
+    val labelMedium: TextStyle = TextStyle(
+        fontFamily = privateFontFamily,
+        fontWeight = FontWeight.Normal,
         fontSize = 12.sp,
         lineHeight = 16.sp
-    )
-){
+    ),
+    val labelSmall: TextStyle = TextStyle(
+        fontFamily = privateFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 10.sp,
+        lineHeight = 14.sp
+    ),
+) {
     constructor(
-       fontFamily: FontFamily,
+        fontFamily: FontFamily,
     ) : this(
         privateFontFamily = fontFamily,
-     )
+    )
 }
 
 val LocalHaloTypography = staticCompositionLocalOf {
