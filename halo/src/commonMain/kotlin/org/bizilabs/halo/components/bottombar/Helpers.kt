eb5020fae47627b0406bc027a7a36f354149c74c
Waitara <janewaitara99@gmail.com>
2025-08-01 18:56:27 +0300

    Adds HaloBaseBottomBar with an indicator slot

--- halo/src/commonMain/kotlin/org/bizilabs/halo/components/bottombar/Helpers.kt
+++ halo/src/commonMain/kotlin/org/bizilabs/halo/components/bottombar/Helpers.kt
@@ -1,7 +1,9 @@
 package org.bizilabs.halo.components.bottombar
 
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import org.bizilabs.halo.HaloTheme
@@ -43,4 +45,37 @@ object HaloBottomBarItemDefaults {
 enum class HaloBottomBarItemContentOrientation {
     Vertical,
     Horizontal,
+}
+
+// Might we need different classes for the different indicator types
+data class IndicatorProperties(
+    val color: HaloColor,
+    val shape: Shape,
+    val lineHeight: Dp,
+    val pillHeight: Dp,
+    val pillPadding: PaddingValues,
+) {
+    companion object {
+        @Composable
+        fun default() =
+            IndicatorProperties(
+                color =
+                    HaloColor(
+                        container = HaloTheme.colorScheme.primary.weaker,
+                        content = HaloTheme.colorScheme.primary.strong,
+                        border = HaloTheme.colorScheme.primary.strong,
+                    ),
+                shape = HaloTheme.shapes.large,
+                lineHeight = 2.dp,
+                pillHeight = HaloBottomBarItemDefaults.minBottomBarHeight,
+                pillPadding = PaddingValues(4.dp),
+            )
+    }
+}
+
+enum class HaloBottomBarIndicatorType {
+    LineTop,
+    LineBottom,
+    Pill,
+    // TODO Think of more indicators
 }
